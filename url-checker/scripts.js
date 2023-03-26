81a4ec8e8ee0dbfa1c362c9d6c587eb5034adc61
Sunny Capt <sunny.capt@tuta.io>
2023-03-27 01:54:08 +0200

    - remove result on input - seperated server call function - change mocked response

--- url-checker/scripts.js
+++ url-checker/scripts.js
@@ -1,47 +1,64 @@
-const urlInput = document.querySelector('#url');
-const resultDiv = document.querySelector('#result');
+const urlInput = document.querySelector('#url')
+const resultDiv = document.querySelector('#result')
 
-let timeoutId;
+let checkUrlTimeoutId
+let renderResultTimeoutId
 
-urlInput.addEventListener('input', async (event) => {
-  clearTimeout(timeoutId);
-  const inputValue = event.target.value;
-  timeoutId = setTimeout(async () => {
-    try {
-      await checkUrl(inputValue);
-    } catch (error) {
-      console.error(error);
-    }
-  }, 1000);
-});
-
-async function checkUrl(url) {
-  let result;
-
-  if (!url) {
-    resultDiv.innerHTML = '';
-    return;
+function renderResult(msg, timeout=0) {
+  if (!!timeout) {
+    resultDiv.innerHTML = msg
+    return
   }
 
-  const regex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+  renderResultTimeoutId = setTimeout(() => {
+    resultDiv.innerHTML = msg
+  }, timeout)
+}
+
+urlInput.addEventListener('input', async (event) => {
+  clearTimeout(renderResultTimeoutId)
+  clearTimeout(checkUrlTimeoutId)
+
+  resultDiv.innerHTML = ''
+
+  checkUrlTimeoutId = setTimeout(async () => {
+    try {
+      await checkUrl(event.target.value)
+    } catch (error) {
+      console.error(error)
+    }
+  }, 1000)
+})
+
+async function callCheckUrlEndpoint(url) {
+  const data = [
+      {"is_exists": true, "type": "file"},
+      {"is_exists": true, "type": "directory"},
+      {"is_exists": false}
+  ]
+  return data[Math.ceil(Math.random() * data.length)]
+}
+
+async function checkUrl(url) {
+  if (!url) {
+    renderResult('')
+    return
+  }
+
+  const regex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i
   if (!regex.test(url)) {
-    resultDiv.innerHTML = 'Invalid URL format.';
-    return;
+    renderResult('Invalid URL format.', 500)
+    return
   }
 
   try {
-    const response = await fetch(`/url-checker/check?url=${url}`);
-    const data = await response.json();
-    console.log(data);
-    result = `${data.is_exists?'exists':'does not exist'}`
-    if (!!data.is_exists && !!data.type)
-      result = `the ${data.type} ${result}`;
+    let respone = await callCheckUrlEndpoint(url)
+    let result = `${respone.is_exists?'exists':'does not exist'}`
+    if (!!respone.type)
+      result = `the ${respone.type} ${result}`
+    renderResult(result, 500)
   } catch (error) {
-    console.error(error);
-    result = 'Internal error';
+    console.error(error)
+    renderResult('Try again...', 500)
   }
-
-  setTimeout(() => {
-    resultDiv.innerHTML = result;
-  }, 500);
 }
